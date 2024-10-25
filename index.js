a3095c5c357725aec056b2da416a57b9231c56da
gozonjoedaimar <gozonjoedaimar@gmail.com>
2024-10-25 15:41:57 +0800

    Define functions

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const cron = require('node-cron')
 const express = require('express')
 const app = express()
 
-let tasks = {}
+// tasks store
+globalThis.tasks = {}
 
+// convert timeout to primitive (integer)
 function toPrimitive(timeout) {
 	return timeout[Symbol.toPrimitive]?.();
 }
 
+// create task id
 function createTaskId() {
 	const timeout = setTimeout(() => true, 0);
 	const id = toPrimitive(timeout);
@@ -15,12 +18,14 @@ function createTaskId() {
 	return id;
 }
 
+// register task to global tasks
 function registerTask(task, uid) {
 	const id = uid || createTaskId();
 	tasks[id] = task;
 	return id;
 }
 
+// destroy task from global tasks
 function destroyTask(id) {
 	if (tasks[id]) {
 		tasks[id]?.destroy();
@@ -28,22 +33,26 @@ function destroyTask(id) {
 	}
 }
 
+// page route (index)
 app.get('/', function(req, res) {
 	res.send('Hello World')
+
+	// task key
 	let task_key = null;
-	// execute cron every 12 secs
+
+	// execute cron every 5 secs
 	const task = cron.schedule('*/5 * * * * *', () => {
-		console.log('running a task every 5 seconds')
-		console.log('task_key', task_key);
+		console.log('running a task every 5 seconds', task_key)
 	})
 
+	// register task and get id
 	task_key = registerTask(task);
 	console.log('started task_key', task_key);
 
 	setTimeout(() => {
 		// destroy the task after 15 secs
 		destroyTask(task_key);
-		console.log('task destroyed');
+		console.log('task destroyed', task_key);
 	}, 15_000);
 })
 
@@ -53,6 +62,7 @@ setInterval(() => {
 	console.log('tasks', Object.keys(tasks));
 }, 1_000);
 
+// start server
 const port = process.env.PORT || 3100
 app.listen(port)
 console.log('Server is running on port ' + port)
